58f87d8e7493a64fa65d72980ea2d5b3db54d9ab
yanwenwu90 <1239181126@qq.com>
2023-05-05 23:49:28 +0800

    day 1

---
+++ src/APP.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class APP extends Component {
+  render() {
+    return (
+      <div>APP</div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 引入react核心库
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 引入reactDOM
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 引入APP
+import APP from "./APP";
+
+ReactDOM.render(<APP />, document.getElementById("root"));
