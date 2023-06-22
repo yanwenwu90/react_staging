04b19a158ef3b0fec4e1e74f3acc13c155117896
yanwenwu90 <1239181126@qq.com>
2023-06-23 00:16:09 +0800

    github search test

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,58 +3,31 @@ import "./index.css";
 
 export default class List extends Component {
   render() {
+    const { users, isFirst, isLoading, err } = this.props;
     return (
       <div className="row">
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img
-              alt="head_portrait"
-              src="https://img2.baidu.com/it/u=678147894,3846377462&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img
-              alt="head_portrait"
-              src="https://img2.baidu.com/it/u=678147894,3846377462&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img
-              alt="head_portrait"
-              src="https://img2.baidu.com/it/u=678147894,3846377462&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img
-              alt="head_portrait"
-              src="https://img2.baidu.com/it/u=678147894,3846377462&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img
-              alt="head_portrait"
-              src="https://img2.baidu.com/it/u=678147894,3846377462&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
+        {isFirst ? (
+          <h2>欢迎使用，输入关键字，随后点击搜索</h2>
+        ) : isLoading ? (
+          <h2>Loading...</h2>
+        ) : err ? (
+          <h2 style={{ color: "red" }}>{err}</h2>
+        ) : (
+          users.map((userObj) => {
+            return (
+              <div key={userObj.id} className="card">
+                <a href={userObj.html_url} target="_blank" rel="noreferrer">
+                  <img
+                    alt="head_portrait"
+                    src={userObj.avatar_url}
+                    style={{ width: "100px" }}
+                  />
+                </a>
+                <p className="card-text">{userObj.login}</p>
+              </div>
+            );
+          })
+        )}
       </div>
     );
   }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,14 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Search extends Component {
+  Search = () => {
+    const {
+      keyElement: { value: keyWord },
+    } = this;
+
+    this.props.updateAppState({ isFirst: false, isLoading: true });
+    // 发送网络请求
+    axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
+      (response) => {
+        this.props.updateAppState({
+          users: response.data.items,
+          isLoading: false,
+        });
+      },
+      (error) => {
+        this.props.updateAppState({
+          err: error.message,
+          isLoading: false,
+        });
+      }
+    );
+  };
+
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">搜索Github用户</h3>
         <div>
-          <input type="text" placeholder="输入关键词点击搜索" />
+          <input
+            ref={(c) => {
+              this.keyElement = c;
+            }}
+            type="text"
+            placeholder="输入关键词点击搜索"
+          />
           &nbsp;
-          <button onClick={this.Search}>搜素</button>
+          <button onClick={this.Search}>搜索</button>
         </div>
       </section>
     );
